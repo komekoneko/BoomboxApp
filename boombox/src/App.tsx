de38149961a939167a1d57af856841df4b27c078
komekoneko <kai.s20050701@gmail.com>
2026-08-11 22:08:12 +0900

    add 再生機能追加

--- boombox/src/App.tsx
+++ boombox/src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <>
-      <h1>radio portfolio</h1>
+      <div className='boombox'>
+        <div className='caset'>
+          <div className= { `reel ${isPlaying? 'playing': ""}`}></div>
+          <div className= { `reel ${isPlaying? 'playing': ""}`}></div>
+        </div>
+        <div className='music'></div>
+        <div className='button'>
+          {/* 再生・停止ボタン */}
+          <button onClick={() => setIsPlaying(!isPlaying)}>{ isPlaying ? "⏸": "▶" }</button>
+        </div>
+      </div>
     </>
   )
 }
