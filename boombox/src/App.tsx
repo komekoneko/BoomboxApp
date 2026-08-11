752be5f0541131d851c246fee9f9bd0aacb9fc69
komekoneko <kai.s20050701@gmail.com>
2026-08-11 23:31:05 +0900

    add 曲タイプ、オブジェクト生成

--- boombox/src/App.tsx
+++ boombox/src/App.tsx
@@ -1,42 +1,48 @@
-import './App.css'
-import { useState,useRef } from 'react'
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
-
   const [isPlaying, setIsPlaying] = useState(false);
-  const [trackName, setTrackName] = useState("2_23_AM")
-  const audioRef = useRef<HTMLAudioElement>(new Audio('/2_23_AM.mp3'))
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(new Audio("/2_23_AM.mp3"));
 
-const togglePlay = () => {
-  if(isPlaying) {
-    audioRef.current.pause();
-    setIsPlaying(false);
-  }else{
-    audioRef.current.play();
-    setIsPlaying(true);
+  type Track = {
+     id: number, title: string, src: string, content: string 
   }
-}
+  
+
+  const track: Track[] = [
+    { id: 1, title: "2_23_AM", src: "/2_23_AM.mp3", content: "温泉TodoApp" },
+  ];
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  };
   return (
     <>
-      <div className='boombox'>
-
-        <div className='caset'>
-          <div className= { `reel ${isPlaying? 'playing': ""}`}></div>
-          <div className= { `reel ${isPlaying? 'playing': ""}`}></div>
+      <div className="boombox">
+        <div className="caset">
+          <div className={`reel ${isPlaying ? "playing" : ""}`}></div>
+          <div className={`reel ${isPlaying ? "playing" : ""}`}></div>
         </div>
 
-        <div className='music'>
-            <p>{trackName}</p>
+        <div className="music">
+          <p>{track[currentIndex].title}</p>
         </div>
 
-        <div className='button'>
+        <div className="button">
           {/* 再生・停止ボタン */}
-          <button onClick={togglePlay}>{ isPlaying ? "⏸": "▶" }</button>
+          <button onClick={togglePlay}>{isPlaying ? "⏸" : "▶"}</button>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
